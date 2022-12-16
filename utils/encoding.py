41fd74c21b6333f6782331a483f51db606486217
Omar Crosby <omar.crosby@gmail.com>
2022-12-16 17:07:39 -0500

    feat(encoding): working on a turning machine class

--- utils/encoding.py
+++ utils/encoding.py
@@ -3,6 +3,49 @@ from utils.prime import prime_factors
 from utils.prime import sieve_of_eratosthenes
 from utils.prime import next_prime
 from collections import deque
+from utils.tape import ReadTape, WriteTape
+
+
+class TuringMachine:
+    state: int
+    itape: ReadTape
+    otape: WriteTape
+
+    def __init__(self, data: str):
+        self.state = 0
+        self.itape = ReadTape(data)
+        self.otape = WriteTape()
+        self.transitions = {
+            0: {
+                "(": ("(", 1, "R"),
+                ".": (".", 2, "R"),
+                ")": (")", 3, "R"),
+            },
+
+        }
+
+    def step(self):
+        if self.itape.eof():
+            return False
+
+        current_char = self.itape.read()
+        transition = self.transitions[self.state][current_char]
+        self.otape.write(transition[0])
+        self.state = transition[1]
+
+        if transition[2] == "L":
+            self.itape.retract()
+        elif transition[2] == "R":
+            self.itape.read()
+
+        return True
+
+    def run(self):
+        while self.step():
+            pass
+
+        return self.output_tape
+
 
 
 def count_factors(factor: int, factor_list: list) -> int:
@@ -45,6 +88,74 @@ def encode(data: int) -> str:
         buffer += encode(exponent)
 
     return f"({buffer})"
+
+
+class ValidationError(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
+def next_exponent(itape: ReadTape) -> int:
+    exponent = 1
+    stack = deque()
+
+    while not itape.eof():
+        char = itape.read()
+
+        if char == ".":
+            return None
+
+        if char == "(":
+            stack.append(char)
+        elif char == ")":
+            stack.pop()
+
+            if len(stack) == 0:
+                break
+            else:
+                exponent += 1
+
+    return exponent
+
+
+def experiment(data: str) -> list:
+    """Return a list of tuples containing the prime factorization.
+    The first element of each tuple is the prime factor, and the second element is the exponent.
+
+    :param data:
+    :return:
+    """
+    char_stack = deque()
+
+    itape = ReadTape(data)
+    factorization = []
+
+    prime_factor = 2
+    exponent = 0
+
+    while not itape.eof():
+        char = itape.read()
+
+        if char == ".":
+            if itape.eof():
+                return []
+        elif char == '(':
+            char_stack.append(char)
+        elif char == ')':
+            if len(factorization) == 0:
+                factorization.append((prime_factor, 0))
+            else:
+                factorization[-1] = (factorization[-1][0], factorization[-1][1] + 1)
+
+            char_stack.pop()
+
+            if len(char_stack) == 0:
+                exponent = 0
+                prime_factor = next_prime(prime_factor)
+
+    return factorization
+
 
 
 def decode_exponents(data: str) -> list:
