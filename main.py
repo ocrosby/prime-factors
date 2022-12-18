2c1cd8183ea1fa63d35eae259c90043a9bce7f02
Omar Crosby <omar.crosby@gmail.com>
2022-12-18 06:05:23 -0500

    feat(main): updating main to accept multiple lines of input from stdin

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
+
 from utils.encoding import encode
 
 if __name__ == "__main__":
-    buffer = input()
-    while buffer != 'exit':
-        value = int(buffer)
+    lines = sys.stdin.readlines()
+
+    for line in lines:
+        value = int(line)
         encoded_value = encode(value)
 
         print(encoded_value)
-        buffer = input()
