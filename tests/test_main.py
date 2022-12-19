cd6ab8ed613fbca431c3282879a54713ff077e68
Omar Crosby <omar.crosby@gmail.com>
2022-12-19 08:27:05 -0500

    chore: adding more tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -51,6 +51,62 @@ class TestSieveOfAtkin:
         assert sieve_of_atkin(100) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 
+class TestSieveOfAtkin:
+    def test_0(self):
+        assert sieve_of_atkin(0) == []
+
+    def test_1(self):
+        assert sieve_of_atkin(1) == []
+
+    def test_2(self):
+        assert sieve_of_atkin(2) == [2]
+
+    def test_3(self):
+        assert sieve_of_atkin(3) == [2, 3]
+
+    def test_4(self):
+        assert sieve_of_atkin(4) == [2, 3]
+
+    def test_5(self):
+        assert sieve_of_atkin(5) == [2, 3, 5]
+
+    def test_6(self):
+        assert sieve_of_atkin(6) == [2, 3, 5]
+
+    def test_7(self):
+        assert sieve_of_atkin(7) == [2, 3, 5, 7]
+
+    def test_8(self):
+        assert sieve_of_atkin(8) == [2, 3, 5, 7]
+
+    def test_9(self):
+        assert sieve_of_atkin(9) == [2, 3, 5, 7]
+
+    def test_10(self):
+        assert sieve_of_atkin(10) == [2, 3, 5, 7]
+
+    def test_11(self):
+        assert sieve_of_atkin(11) == [2, 3, 5, 7, 11]
+
+    def test_12(self):
+        assert sieve_of_atkin(12) == [2, 3, 5, 7, 11]
+
+    def test_13(self):
+        assert sieve_of_atkin(13) == [2, 3, 5, 7, 11, 13]
+
+    def test_14(self):
+        assert sieve_of_atkin(14) == [2, 3, 5, 7, 11, 13]
+
+    def test_15(self):
+        assert sieve_of_atkin(15) == [2, 3, 5, 7, 11, 13]
+
+    def test_16(self):
+        assert sieve_of_atkin(16) == [2, 3, 5, 7, 11, 13]
+
+    def test_30(self):
+        assert sieve_of_atkin(30) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+
 class TestSieveOfEratosthenes:
     def test_0(self):
         assert sieve_of_eratosthenes(0) == []
